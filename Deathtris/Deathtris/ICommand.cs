3c62b505f91b410f941e7b152c4038e27cf820cf
Simon Cordes <simon.holm.42@gmail.com>
2019-03-27 11:23:07 +0100

    InputHandler, movement and ICommand

---
+++ Deathtris/Deathtris/ICommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Deathtris.Components;
+
+namespace Deathtris
+{
+    interface ICommand
+    {
+        void Execute(Player player);
+    }
+}
